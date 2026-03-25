49c998e28c12a8a2d8753d580ba045f8cb535b49
zbq <hello@zbq.cn>
2026-03-25 21:33:02 +0800

    fk!

---
+++ src/main/java/com/meategg/entity/User.java
@@ -0,0 +1,7 @@
+package com.meategg.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+
+@TableField()
+public class User {
+}
--- src/main/java/com/meategg/service/userServiceimpl.java
+++ src/main/java/com/meategg/service/userServiceimpl.java
@@ -2,6 +2,7 @@ package com.meategg.service;
 
 import com.meategg.Utils.JwtUtils;
 import com.meategg.entity.Result;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javax.annotation.Resource;
 
@@ -12,8 +13,11 @@ public class userServiceimpl implements userService {
 private JwtUtils jwtUtils;
     @Override
     public Result login(String username, String password) {
-    User user = query().eq("username", username).eq("password", password);
+        User user = query().eq("username", username).one();
     if(user == null){return Result.fail("用户名或密码错误");}
+        if (!BCrypt.checkpw(password, user.getPassword())) {
+            return Result.fail("用户名或密码错误");
+        }
     String jwt = jwtUtils.createJwt(username);
     return Result.ok(jwt);
     }
