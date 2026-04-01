faa0bf1fcb665c83effcb1f83a4987622e03691e
zbq <hello@zbq.cn>
2026-04-01 20:13:13 +0800

    fk!

--- src/main/java/com/meategg/entity/User.java
+++ src/main/java/com/meategg/entity/User.java
@@ -1,7 +1,19 @@
 package com.meategg.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
-@TableField()
+@TableName("user")
+@Data
 public class User {
+    @TableField("id")
+    private Integer id;
+    @TableField("username")
+    private String username;
+    @TableField("password")
+    private String password;
+    @TableField("created_at")
+    private String created_at;
+
 }
--- src/main/java/com/meategg/service/userService.java
+++ src/main/java/com/meategg/service/userService.java
@@ -1,9 +1,13 @@
 package com.meategg.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.meategg.entity.Result;
+import com.meategg.entity.User;
+import com.meategg.mapper.UserMapper;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface userService{
+public interface userService extends IService<User> {
     public Result login(String username, String password);
 }
