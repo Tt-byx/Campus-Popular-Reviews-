41f000ee11c07cbb33072b8a75867f1c8f8fb4cf
zbq <hello@zbq.cn>
2026-04-15 20:48:39 +0800

    fk!

---
+++ src/main/java/com/meategg/entity/CommentContent.java
@@ -0,0 +1,23 @@
+package com.meategg.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("comment_content")
+public class CommentContent {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    @TableField("post_id")
+    private Long postId;
+
+    @TableField("comment_id")
+    private Long commentId;
+
+    @TableField("content")
+    private String content;
+}
