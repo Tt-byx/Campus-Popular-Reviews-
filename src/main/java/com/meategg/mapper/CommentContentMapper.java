6ec6b1cfbb6a412219e8967935525c87773281f9
zbq <hello@zbq.cn>
2026-04-15 20:48:39 +0800

    fk!

---
+++ src/main/java/com/meategg/mapper/CommentContentMapper.java
@@ -0,0 +1,15 @@
+package com.meategg.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.meategg.entity.CommentContent;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CommentContentMapper extends BaseMapper<CommentContent> {
+
+    List<CommentContent> listByPostIdAndCommentIds(@Param("postId") Long postId, @Param("commentIds") List<Long> commentIds);
+}
